69c1cf1f80251d25de50266e43c5a80feb0ff555
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-05-29 22:45:32 +0100

    fixed! i think

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 
-export const createAgent = () => {
+export const CreateAgent = () => {
     const agent = axios.create({
     
         baseURL: 'https://sbxapi.smoothballot.com',
