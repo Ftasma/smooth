ecd9457d719a0e1a627bf776d85296ab8359ba5e
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-05-29 22:06:07 +0100

    bug

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,13 +1,14 @@
+"use client"
 import axios from "axios";
 import {cookies} from "next/headers";
 import { useCookies } from "react-cookie";
 
 
-const [ cookie, setCookie ] = useCookies();
 export const agent = axios.create({
-
+    
     baseURL: 'https://sbxapi.smoothballot.com',
 });
+const [ cookie, setCookie ] = useCookies();
 agent.interceptors.request.use(config => {
     const token = cookie.token;
     if (token) {
