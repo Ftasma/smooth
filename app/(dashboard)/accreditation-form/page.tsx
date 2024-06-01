359e612ac5785378d19ab5863f27366eadc51782
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-06-01 22:38:50 +0100

    padding in accreditation form

--- app/(dashboard)/accreditation-form/page.tsx
+++ app/(dashboard)/accreditation-form/page.tsx
@@ -12,7 +12,7 @@ const AccreditionForm = () => {
   return (
     <section className='h-[100vh] w-full'>
          <Link href='/voters-acquisition'><button className=' bg-gray-300  rounded-full p-1 text-gray-700 font-thin mt-[7%] ml-[7%]'><ChevronLeft/></button></Link>
-         <div className='dashboard-dimensions px-2 md:overflow-y-auto no-scrollbar'>
+         <div className='dashboard-dimensions px-5 md:overflow-y-auto no-scrollbar'>
             <h1 className=' text-2xl font-medium'>Create Accreditation Form</h1>
             <p className='text-sm text-gray-500'>Personalize your accreditation form</p>
             <div className={cn('mt-3 h-10 flex justify-between px-5 w-full bg-[#F6F6F6]', !isEditing&&"bg-[#C92929] !text-white")}>
@@ -31,23 +31,23 @@ const AccreditionForm = () => {
                 <Input placeholder='Form title' className=' bg-[#EAEAEA] border-none rounded placeholder:text-[#57595A]'/>
                 <Input placeholder='Form Description' className='h-[5rem] bg-[#EAEAEA] border-none rounded placeholder:text-[#57595A]'/>
             </aside>
-            <aside className=' h-[30%] flex flex-col  border-2 space-y-3 rounded px-6 py-3  '>
+            <aside className=' h-[30%] flex flex-col  border-2 space-y-5 rounded px-6 py-3  '>
               <p className=' place-self-start text-[#1F2223]'>Label</p>
               <div className=' flex justify-between items-center'>
                 <Input placeholder='Form title' className=' bg-[#EAEAEA] border-none w-[50%] rounded placeholder:text-[#57595A]'/>
-                <div className=' w-[30%] border-2'>
-                <select name="" className='text-[#57595A] w-[30%] border-none' id="">
+                <div className='md:w-[30%] w-[45%] border-2'>
+                <select name="" className='text-[#57595A] md:w-[30%] w-[45%] border-none' id="">
                   <option value="">Short Answer</option>
                 </select>
                 </div>
               </div>
                 <div className=' flex justify-between'>
                   <span className='flex gap-3 items-center'><input type='checkbox' className='w-5 h-5'/><p>Required</p></span>
-                  <button className='flex gap-1 border-2 p-1 border-[#A92323] text-[#A92323]'><Trash className=' text-2xl'/><p className=' font-bold'>Delete</p></button>
+                  <button className='flex gap-1 border-[2px] p-1 border-[#A92323] text-[#A92323] items-center'><Trash className='' size={18}/><p className='text-[0.8rem] font-bold'>Delete</p></button>
                 </div>
             </aside>
                 <Button className='place-self-start border-2 border-[#0654B0] text-[#0654B0] rounded'><Plus/>Add more questions</Button>
-                <Button className=' place-self-center bg-[#0654B0] text-white mt-3 w-[70%] rounded'>Get link <ArrowRight/></Button>
+                <Button className=' place-self-center bg-[#0654B0] text-white mt-3 w-[85%] rounded'>Get link <ArrowRight/></Button>
          </div>
     </section>
   )
