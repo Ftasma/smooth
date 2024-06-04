cf8682d52d349882b549f71ec70479eea3e6c3d8
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-06-04 22:37:49 +0100

    gg

--- app/(dashboard)/create-election-2/page.tsx
+++ app/(dashboard)/create-election-2/page.tsx
@@ -29,7 +29,7 @@ const CreateElection2 = () => {
     <div className=' h-screen w-full '>
         
         <Link href='/dashboard'><button className=' bg-gray-300  rounded-full p-1 text-gray-700 font-thin mt-[7%] ml-[7%]'><ChevronLeft/></button></Link>
-        <aside className='dashboard-dimensions overflow-y-auto'>
+        <aside className='dashboard-dimensions'>
             <h1 className='text-[#1F2223] text-2xl font-bold '>Create new election</h1>
             <p className='text-[#57595A]'>Fill in the details below⚡</p>
             <div className='flex justify-around'>
@@ -38,18 +38,20 @@ const CreateElection2 = () => {
                 <span className=' z-50 rounded-full flex items-center justify-center bg-[#0654B0] p-3 px-6 w-9 text-white'><p className=' place-self-center'>3</p></span>
             </div>
             <div className=' border-[#BCBCBC] border w-[65%] mx-auto border-dotted -mt-8'/>
+            <div className=' overflow-y-auto no-scrollbar ml-[20%] w-full place-self-center'>
             {query?.data?.data?.data?.candidates?.map((candidate:any)=>(
-            <div key={candidate.id} className='mt-6 place-self-center h-16 flex justify-between  items-center px-3 rounded-xl w-[80%] border-2 border-blue-500 '>
-              <div className=' w-[50%] flex justify-around items-center'>
+              <div key={candidate.id} className='mt-6 place-self-center h-16 flex justify-between  items-center px-3 rounded-xl w-[80%] border-2 border-blue-500 '>
+              <div className=' w-[70%] flex justify-around items-center'>
                 <Image height={22} width={45} className=' object-cover' src={plus} alt='Candidate image'/>
                 <h1>{candidate.name}</h1>
                 <p className=' text-xs bg-[#D9D9D9] text-black rounded px-1'>{candidate.ElectionPost.title}</p>
               </div>
-              <div>
-                <Trash size={16}/>
+              <div className=' pr-2 pb-1'>
+                <h2 className=' text-gray-400'>...</h2>
               </div>
              </div>
             ))}
+            </div>
             <aside className=' mt-3 space-y-3'>
                 <div className='original-border mx-auto !h-[58px] !w-[80%] !border-[#BCBCBC] justify-center items-center flex gap-4'>
                 <PlusCircle onClick={()=>setShowModal(true)} className='cursor-pointer text-[#0654B0]' size={20}/>
