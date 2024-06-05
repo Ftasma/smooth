a7985fd327454542b6b5a41a3a5f3f10628fcb11
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-06-05 17:05:11 +0100

    dashboard again

--- app/(election-management)/election-management-dashboard/page.tsx
+++ app/(election-management)/election-management-dashboard/page.tsx
@@ -5,7 +5,7 @@ import { Calendar, ChevronLeft, Clock, Pencil, ToggleLeft } from 'lucide-react';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
-const page = () => {
+const Page = () => {
     const [electionName, setElectionName]= useState("")
     const [electionDate, setElectionDate]= useState("")
     useEffect(() => {
@@ -69,4 +69,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
