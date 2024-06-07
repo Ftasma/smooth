e942ba3f527c72b26e3d375cbd0365d806e92e3b
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-06-07 08:12:12 +0100

    Accordition Fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import UserButton from './UserButton';
 import SmoothLogo from './SmoothLogo';
 const Navbar = () => {
   return (
-    <div className="bg-[#FFFFFF] h-[70px] w-full flex justify-between md:px-14 px-8 items-center shadow-md fixed z-50">
+    <div className="bg-[#FFFFFF] h-[70px] w-full flex justify-between md:px-14 px-8 items-center shadow-md fixed z-[99999999]">
     <SmoothLogo/>
     <UserButton/>
     </div>
