b185f1e52773c64c8af08b8ec0f69a640c811e26
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-06-09 19:59:50 +0100

    loader on dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,44 +1,44 @@
-"use client"
+"use client";
 
-import { BASE_URL } from '@/lib/endpoints'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import DisplayDashboard from './_components/DisplayDashboard'
-import FirstTimeDisplay from './_components/FirstTimeDisplay'
+import { BASE_URL } from '@/lib/endpoints';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import DisplayDashboard from './_components/DisplayDashboard';
+import FirstTimeDisplay from './_components/FirstTimeDisplay';
+import { Loader2 } from 'lucide-react';
 
 const Dashboard = () => {
-  const fetchData=async()=>{
-    return await axios.get(`${BASE_URL}/election`)
-    
- }
- const query = useQuery({
-  queryFn: fetchData,
-  queryKey: ['next'],
- })
-//  console.log(query?.data?.data?.data);
- 
-const isElection = query?.data?.data?.data?.elections[0]?.name
-console.log(isElection);
-// console.log(isElection[0]);
+  const fetchData = async () => {
+    return await axios.get(`${BASE_URL}/election`);
+  };
 
+  const query = useQuery({
+    queryFn: fetchData,
+    queryKey: ['next'],
+  });
 
+  const isElection = query?.data?.data?.data?.elections[0]?.name;
+  console.log(isElection);
 
+  if (query.isLoading) {
+    return (<div className="flex justify-center items-center h-screen">
+    <Loader2 className="animate-spin" size={48} />
+  </div>)
+  }
+
+  if (query.isError) {
+    return <div>Error loading data</div>;
+  }
 
   return (
     <section>
-     {isElection ? (
-       <DisplayDashboard/> ) : (
+      {isElection ? (
+        <DisplayDashboard />
+      ) : (
         <FirstTimeDisplay />
       )}
     </section>
-  )
-}
+  );
+};
 
-export default Dashboard
-{/* <div>{query?.data?.data?.data?.elections.map(
-          (election:any)=>{
-            return <div key={election.id}>{election.name}</div>
-          }
-        
-        )}</div>
-      ) */}
+export default Dashboard;
