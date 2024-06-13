febdc8c32e4750244071dbd4af9140b909d08483
Oluwafemi Araoye <127428151+Ftasma@users.noreply.github.com>
2024-06-13 13:38:56 +0100

    Update routing

--- components/SmoothLogo.tsx
+++ components/SmoothLogo.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Image from "next/image"
 import lightlogo from "../public/logolight.png"
+import Link from 'next/link'
 const SmoothLogo = () => {
-  return <Image  className=" w-[110px] h-[110px]" src={lightlogo} alt="logo"/>
+  return <Link href="/dashboard"> <Image  className=" w-[110px] h-[110px]" src={lightlogo} alt="logo"/></Link>
 }
 
 export default SmoothLogo
